9d1a8c90db7202e427e150e8be6230be4ce82312
ozymok1604 <99099122+ozymok1604@users.noreply.github.com>
2023-05-02 23:38:46 +0300

    finished app

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const API_URL = 'http://94.131.246.109:5555/v1/';
+const CLASS_KEY = 2;
+
+const ROUTES = Object.freeze({
+  ATTENDANCE_TABLE: '/',
+  STUDENTS_COUNT: '/students-count',
+});
+
+const ABSENCE = 'H';
+
+export { ROUTES, API_URL, CLASS_KEY, ABSENCE };
